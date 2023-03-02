ad753929560f3c96cf3ca4afb936808d2ec30cca
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-03-02 14:50:43 -0500

    feat: :sparkles: add import aliases, importMeta types

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,13 @@
 import { getViteConfig } from "astro/config";
+import { resolve } from "path";
 
 export default getViteConfig({
+  resolve: {
+    alias: {
+      "@tRPC": resolve(__dirname, "./src/trpc"),
+      "~": resolve(__dirname, "./src"),
+    },
+  },
   test: {
     includeSource: ["src/**/*.{js,ts}"],
     coverage: { reporter: ["html-spa", "text"] },
