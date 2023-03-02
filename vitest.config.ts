ab252503e7270a36195a252a8885c70fab339182
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-03-01 23:34:01 -0500

    refactor: :sparkles: adjust typelaunch for astro

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { getViteConfig } from "astro/config";
+
+export default getViteConfig({
+  test: {
+    includeSource: ["src/**/*.{js,ts}"],
+    coverage: { reporter: ["html-spa", "text"] },
+  },
+  define: {
+    "import.meta.vitest": "undefined",
+  },
+});
