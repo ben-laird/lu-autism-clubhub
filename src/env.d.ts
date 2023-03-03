dfbf7419ef8852c0960c7455111a7512d49148ff
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-03-03 15:27:06 -0500

    feat: :sparkles: renovate environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 /* eslint-disable spaced-comment */
 /// <reference types="astro/client" />
+
+enum Env {
+  DATABASE_URL,
+  TRPC_DEV_URL,
+  TRPC_PROD_URL,
+}
+
+type EnvRecord<T extends string> = Readonly<Record<T, string>>;
+
+type ImportMetaEnv = EnvRecord<keyof typeof Env>;
