fbec8d16651e2ec8913c859a4df6ac6feebb27aa
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-03-03 18:14:14 -0500

    refactor: :recycle: use environment variables in httpBatchLink

--- src/trpc/client.ts
+++ src/trpc/client.ts
@@ -2,9 +2,15 @@ import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import SuperJSON from "superjson";
 import type { AppRouter } from "./server";
 
+const {
+  DEV: inDev,
+  TRPC_DEV_URL: devUrl,
+  TRPC_PROD_URL: prodUrl,
+} = import.meta.env;
+
 const tRPC = createTRPCProxyClient<AppRouter>({
-  links: [httpBatchLink({ url: "http://localhost:3000/api/trpc" })],
+  links: [httpBatchLink({ url: inDev ? devUrl : prodUrl })],
   transformer: SuperJSON,
 });
 
-export default tRPC
+export default tRPC;
