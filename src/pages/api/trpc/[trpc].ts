a9dbb7ec57933c1b17cc821d4788e92eaba9ded0
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-03-03 18:10:10 -0500

    refactor: :recycle: update imports in tRPC endpoint

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,7 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import type { APIRoute } from "astro";
-import { appRouter as router, createContext } from "~/trpc/server";
+import { createContext } from "~/trpc/initTRPC";
+import { appRouter as router } from "~/trpc/server";
 
 export const all: APIRoute = async ({ request: req }) => {
   return fetchRequestHandler({
