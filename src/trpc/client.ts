12f4e9be1c58a3128070fc96e9ec39d501c9e5ef
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-03-04 16:37:13 -0500

    fix: use SITE environment variable in tRPC client

--- src/trpc/client.ts
+++ src/trpc/client.ts
@@ -2,15 +2,7 @@ import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import SuperJSON from "superjson";
 import type { AppRouter } from "./server";
 
-const {
-  DEV: inDev,
-  TRPC_DEV_URL: devUrl,
-  TRPC_PROD_URL: prodUrl,
-} = import.meta.env;
-
-const tRPC = createTRPCProxyClient<AppRouter>({
-  links: [httpBatchLink({ url: inDev ? devUrl : prodUrl })],
+export const trpcClientSide = createTRPCProxyClient<AppRouter>({
+  links: [httpBatchLink({ url: import.meta.env.SITE })],
   transformer: SuperJSON,
 });
-
-export default tRPC;
