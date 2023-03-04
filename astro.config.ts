4427dc21361ff244bea3efff3c4341aa04e6d65d
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-03-04 16:36:05 -0500

    fix: dynamic Astro site deployment

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,19 @@
+import solidJs from "@astrojs/solid-js";
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-import solidJs from "@astrojs/solid-js";
+const {
+  DEV: inDev,
+  VITE_TRPC_DEV_URL: devUrl,
+  VITE_TRPC_PROD_URL: prodUrl,
+} = import.meta.env;
+
+// console.log(`dev: ${devUrl}, prod: ${prodUrl}`);
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [solidJs()]
+  integrations: [solidJs()],
+  adapter: vercel(),
+  site: inDev ? devUrl || "" : prodUrl || "",
 });
