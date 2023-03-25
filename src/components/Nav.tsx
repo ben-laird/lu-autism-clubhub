bbbf0c91fcf81f0dcf004d4f1d9133cc212794e6
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-03-25 02:41:16 -0400

    feat: :sparkles: make nav sticky

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,12 @@
 import { Component, For } from "solid-js";
 
-export const Nav: Component<{ links: { name: string; href: string }[] }> = (
-  props
-) => (
-  <nav class="bg-white px-4 py-2 shadow-sm">
+export interface Link {
+  name: string;
+  href: string;
+}
+
+export const Nav: Component<{ links: Link[] }> = (props) => (
+  <nav class="sticky top-0 bg-white px-4 py-2 shadow-sm">
     <div class="mx-auto flex max-w-screen-xl items-center justify-between">
       <a href="/" class="text-lg font-semibold text-gray-700">
         Club Name
